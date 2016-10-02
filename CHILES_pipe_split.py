0f38db5db7aa66e5375255b77bee60fe177a0b5a
dpisano <djpisano@mail.wvu.edu>
2016-10-02 12:50:50 -0400

    Version 2.0

--- CHILES_pipe_split.py
+++ CHILES_pipe_split.py
@@ -14,12 +14,15 @@ import pylab as pylab
 import re as re
 import sys
 
-# Test if imaging is quicker after  averaging by 2x in time & 4x in frequency
-outputms=ms_active[:-3]+'_calibrated_deepfield.ms'
-targetfile=outputms
+# Set up to save results
+if os.path.exists("FINAL")==False:
+    os.system("mkdir FINAL")
 
 logprint ('Split calibrated deepfield uv data', logfileout='logs/split.log')
 
+# Split with averaging by 2x in time & 4x in frequency
+outputms=ms_active[:-3]+'_calibrated_deepfield.ms'
+targetfile=outputms
     
 default('split')
 vis=ms_active
@@ -31,11 +34,28 @@ width=4
 timebin='16s'
 split()
 
+os.system("mv "+targetfile+" FINAL/.")
+
 # Save calibration tables
+if os.path.exists('antposcal.p')==True:
+    os.system("cp antposcal.p FINAL/.")
+
+os.system("cp gain_curves.g FINAL/.")
+os.system("cp finaldelay.k FINAL/.")
+os.system("cp finalBPcal.b FINAL/.")
+os.system("cp finalphase_scan.gcal FINAL/.")
+os.system("cp finalamp.gcal FINAL/.")
+os.system("cp finalflux.gcal FINAL/.")
+
+# Save flagging commands
+
+os.system("cp manual_flags_*.txt FINAL/.")
 
 # Save Flag tables
 
-# Save flagging commands
+os.system("cp "+ms_active+".flagversions/flags.finalflags FINAL/.")
+os.system("cp "+ms_active+".flagversions/flags.finalfinalflags FINAL/.")
+
 
 
 logprint ("Finished CHILES_pipe_split.py", logfileout='logs/split.log')
